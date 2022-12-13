dfae892f05b68714f3feb3a82bd1dc976ffdf165
Valentin Korneev <v.korneef@gmail.com>
2022-12-13 14:45:39 +0600

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build Status](https://api.cirrus-ci.com/github/korneef/ra42_whatches.svg)](https://cirrus-ci.com/github/korneef/ra42_whatches)
+
+https://korneef.github.io/ra42_whatches
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Card from './components/Card';
+import logo from './logo.svg'
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
